81c191adc1de98b0bbeccea8b8054a774126fd50
Stefan Dunikovskyi <stefan090304@gmail.com>
2022-08-16 15:24:08 +0300

    changes

--- src/components/Feedback/Buttons/Buttons.jsx
+++ src/components/Feedback/Buttons/Buttons.jsx
@@ -1,9 +1,22 @@
-export const Buttons = ({onClickGood,onClickNeutral,onClickBad})=>{
-
-    return <>
-    <h1>Please leave feedback</h1>
-    <button onClick={()=>onClickGood()}>Good</button>
-    <button onClick={()=>onClickNeutral()}>Neutral</button>
-    <button onClick={()=>onClickBad()}>Bad</button>
-    </>   
-}
+import PropTypes from "prop-types";
+import shortid from "shortid";
+export const Buttons = ({ options, onLeaveFeedback }) => {
+  return (
+    <>
+      {options.map((option) => (
+        <button
+          key={shortid.generate()}
+          name={option}
+          type="button"
+          onClick={onLeaveFeedback}
+        >
+          Good
+        </button>
+      ))}
+    </>
+  );
+};
+Buttons.propTypes = {
+  options: PropTypes.array.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
--- src/components/Feedback/Statistics/Statistics.jsx
+++ src/components/Feedback/Statistics/Statistics.jsx
@@ -1,20 +1,35 @@
 import { Notification } from "../Notification/Notification";
+import PropTypes from "prop-types";
 
-export const Statistics =({good,neutral,bad,total,positivePercentage})=>{
-
-    return <>
-    <div><h1> Statistics</h1>
-        {total >0 ?
-          (<div> 
-          <p>Good:{good}</p>
-          <p>Neutral:{neutral}</p>
-          <p>Bad:{bad}</p>
-          <p>Total:{total}</p>
-          <p>Positive feedback: {positivePercentage}%</p></div>)
-              : 
-              (<Notification message="There is no feedback"></Notification>)
-
-        }
-    </div>
+export const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) => {
+  return (
+    <>
+      <div>
+        {total > 0 ? (
+          <div>
+            <p>Good:{good}</p>
+            <p>Neutral:{neutral}</p>
+            <p>Bad:{bad}</p>
+            <p>Total:{total}</p>
+            <p>Positive feedback: {positivePercentage}%</p>
+          </div>
+        ) : (
+          <Notification message="There is no feedback"></Notification>
+        )}
+      </div>
     </>
-}
+  );
+};
+Statistics.propTypes = {
+  good: PropTypes.number,
+  neutral: PropTypes.number,
+  bad: PropTypes.number,
+  total: PropTypes.number,
+  positivePercentage: PropTypes.number,
+};
